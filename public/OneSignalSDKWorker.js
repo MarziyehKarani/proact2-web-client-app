94e51ae72ab2d656a8a0b5bd157706c9643a82e3
marziyeh.karani <marziyeh.karani@engitel.com>
2024-04-08 16:44:09 +0200

    add serviceworker for PWA

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,3 @@
+
+importScripts('/sw.js');
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 
 import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from "./infrastructure/azure/azureB2cConfig";
 import store from "./store"
+import LocalServiceWorkerRegister from "./serviceWorkerRegistration"
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
@@ -22,7 +23,15 @@ const app = (
 ReactDOM.render(app, document.getElementById("root"))
 //serviceWorker.unregister()
 
-serviceWorker.register()
+//serviceWorker.register()
+LocalServiceWorkerRegister();
+
+
+/* if ("serviceWorker" in navigator) {
+  window.addEventListener('load',() => {
+    navigator.serviceWorker.register('/serviceWorker.js');
+  });
+} */
 
 
 /* if ('serviceWorker' in navigator) {
