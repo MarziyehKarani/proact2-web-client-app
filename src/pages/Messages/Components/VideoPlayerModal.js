29d07e7263cd7b08dd57066688648218729b616f
marziyeh.karani <marziyeh.karani@engitel.com>
2024-09-25 11:05:01 +0200

    open video in fullscreen mode

--- src/pages/Messages/Components/VideoPlayerModal.js
+++ src/pages/Messages/Components/VideoPlayerModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect , useRef } from "react";
 import { Modal, Row, Col, Audio } from "reactstrap";
 import { LoadingSpinner } from "../../../components/Common/LoadingSpinner";
 import {getAttachmentSasUriApi} from "../../../infrastructure/services/network/apiCalls/attachmentApiService";
@@ -8,11 +8,19 @@ import { apiErrorToast } from "../../../helpers/toastHelper";
 export const VideoPlayeModal = ({ isOpen, closeCallback, messageId }) => {
 
     const [sasUri, setSasUri] = useState(null);
+    const iframeRef = useRef(null);
+
     useEffect(() => {
         if (isOpen) {
             LoadUrl();
         }
     }, [isOpen]);
+
+    useEffect(() => {
+        if (sasUri) {
+            openFullscreen();
+        }
+    }, [sasUri]);
 
     function LoadUrl() {
         if (messageId) {
@@ -27,6 +35,23 @@ export const VideoPlayeModal = ({ isOpen, closeCallback, messageId }) => {
     function closeModalHandler() {
         closeCallback();
     }
+
+    const openFullscreen = () => {
+        console.log("openFullscreen");
+        const iframe = iframeRef.current;
+        console.log(iframe);
+        if (iframe) {
+            if (iframe.requestFullscreen) {
+                iframe.requestFullscreen();
+            } else if (iframe.mozRequestFullScreen) { // Firefox
+                iframe.mozRequestFullScreen();
+            } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+                iframe.webkitRequestFullscreen();
+            } else if (iframe.msRequestFullscreen) { // IE/Edge
+                iframe.msRequestFullscreen();
+            }
+        }
+    };
 
     return (
         <Modal
@@ -51,6 +76,7 @@ export const VideoPlayeModal = ({ isOpen, closeCallback, messageId }) => {
                 {sasUri ?
                     <div className="embed-responsive embed-responsive-16by9 ratio ratio-16x9">
                         <iframe
+                            ref={iframeRef}
                             className="embed-responsive-item"
                             allowFullScreen
                             allow='autoplay'
