bd383cdef0e619192df44f2bb32e0f854eb3a124
marziyeh.karani <marziyeh.karani@engitel.com>
2025-03-06 17:01:28 +0100

    added expire time for local storage data

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,78 @@
 
 
-const CACHE_NAME='proact2-dev-cache';
-const PRE_CACHED_RESOURCES = ["/","index.html"];
+ const CACHE_NAME_toRemove='proact2-dev-cache';
+const PRE_CACHED_RESOURCES = ["offline.html","index.html"]; 
+
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+const OFFLINE_URL = "offline.html";
 
 self.addEventListener('install', e => {
+  console.log('installing service worker!!');
+  e.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+          return cache.addAll(
+              PRE_CACHED_RESOURCES
+            )
+          .then(() => self.skipWaiting());
+      })
+  )
+})
+
+  self.addEventListener("activate", (event) => {
+    console.log('activating service worker!!');
+    caches.delete(CACHE_NAME_toRemove);
+
+    event.waitUntil(
+    self.clients.claim()
+    );
+  });
+
+
+  self.addEventListener("fetch", (event) => {
+    // Only call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
+      event.respondWith(
+        (async () => {
+          try {
+            // First, try to use the navigation preload response if it's
+            // supported.
+/*             const preloadResponse = await event.preloadResponse;
+            if (preloadResponse) {
+              return preloadResponse;
+            } */
+  
+            // Always try the network first.
+            const networkResponse = await fetch(event.request);
+            return networkResponse;
+          } catch (error) {
+            // catch is only triggered if an exception is thrown, which is
+            // likely due to a network error.
+            // If fetch() returns a valid HTTP response with a response code in
+            // the 4xx or 5xx range, the catch() will NOT be called.
+            console.log("Fetch failed; returning offline page instead.", error);
+  
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(OFFLINE_URL);
+            return cachedResponse;
+          }
+        })()
+      );
+    }
+  
+    // If our if() condition is false, then this fetch handler won't
+    // intercept the request. If there are any other fetch handlers
+    // registered, they will get a chance to call event.respondWith().
+    // If no fetch handlers call event.respondWith(), the request
+    // will be handled by the browser as if there were no service
+    // worker involvement.
+  });
+
+
+
+
+/* self.addEventListener('install', e => {
     console.log('installing service worker!!');
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -13,62 +82,21 @@ self.addEventListener('install', e => {
             .then(() => self.skipWaiting());
         })
     )
-})
+}) */
 
-self.addEventListener('active', event => {
+/* self.addEventListener('active', event => {
     console.log('activating service worker!!');
     event.waitUntil(
     self.clients.claim()
     );
-})
+}) */
 
 
-/* self.addEventListener('fetch', function (event) {
 
-    if (event.request.method === 'POST' || (event.request.method === 'GET' && event.request.url.indexOf('Users/me') > -1)) {
-        event.respondWith(fetch(event.request));
-        return;
-    }
 
-    if (event.request.method === 'GET') {
-
-        if(navigator.onLine){
-            var fetchRequest=event.request.clone();
-            return fetch(fetchRequest).then(
-                function(response){
-                    if(!response || response.status !== 200 || response.type!== 'basic'){
-                        return response;
-                    }
-    
-                    var responseToCache= response.clone();
-    
-                    caches.open(CACHE_NAME)
-                    .then(function(cache){
-                        cache.put(event.request,responseToCache);
-                    });
-    
-                    return response
-                }
-            )
-    
-        }  else {
-            event.respondWith(
-                caches.open(CACHE_NAME).then(function (cache) {
-                  return cache.match(event.request).then(function (response) {
-                    return response || fetch(event.request).then(function (response) {
-                      cache.put(event.request, response.clone());
-                      return response;
-                    });
-                  });
-                })
-            
-              );
-        }
-    }
-  }); */
   
 
-    self.addEventListener('fetch', async (event) => {
+    /* self.addEventListener('fetch', async (event) => {
         const { request } = event;
     
         // Handle POST requests and specific GET requests immediately
@@ -81,8 +109,8 @@ self.addEventListener('active', event => {
         else if (request.method === 'GET') {
             if (navigator.onLine) {
                 try {
-                    const response = await fetch(request.clone());
-                    if (response && response.status === 200 && response.type === 'basic') {
+                    const response = await fetch(request);
+                    if (response && response.ok && response.type === 'basic') {
                         const responseToCache = response.clone();
                         caches.open(CACHE_NAME).then(cache => {
                          cache.put(request, responseToCache);
@@ -91,7 +119,7 @@ self.addEventListener('active', event => {
                     return response;
                 } catch (error) {
                     console.error('Fetch failed:', error);
-                    // Optionally return a fallback response here
+                    
                 }
             } else {
                 try {
@@ -111,76 +139,8 @@ self.addEventListener('active', event => {
                 }
             }
         }
-    });
+    }); */
     
 
 
 
-
-/* self.addEventListener('fetch', function(event) {
-    console.log(`fetching ${event.request.url}`);
-    // Exclude POST requests from caching
-    if (event.request.method === 'POST') {
-        event.respondWith(fetch(event.request));
-        return;
-    }
-    
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-         
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            // Clone the request to make a fetch
-            var fetchRequest = event.request.clone();
-
-            return fetch(fetchRequest).then(function(response) {
-                // Check if we received a valid response
-                if (!response || response.status !== 200 || response.type !== 'basic') {
-                    return response;
-                }
-                // Clone the response to put it in cache
-                var responseToCache = response.clone();
-                caches.open(CACHE_NAME).then(function(cache) {
-                    cache.put(event.request, responseToCache);
-                });
-                return response;
-            }); 
-        })
-    );
-}); */
-
-/* self.addEventListener('fetch',function(event){
-    console.log('fetching ${event.request.url}');
-    if(navigator.onLine){
-        var fetchRequest=event.request.clone();
-        return fetch(fetchRequest).then(
-            function(response){
-                if(!response || response.status !== 200 || response.type!== 'basic'){
-                    return response;
-                }
-
-                var responseToCache= response.clone();
-
-                caches.open(CACHE_NAME)
-                .then(function(cache){
-                    cache.put(event.request,responseToCache);
-                });
-
-                return response
-            }
-        )
-
-    }  else {
-        event.respondwith(
-            caches.match(event.request)
-            .then(function(response){
-
-                if(response){
-                    return response;
-                }
-            })
-        );
-    }
-}); */
