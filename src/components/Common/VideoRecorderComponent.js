834a8e08624d9ce7d9811cd7be27597fa34dde46
marziyeh.karani <marziyeh.karani@engitel.com>
2025-04-11 14:29:04 +0200

    fixed video issue

--- src/components/Common/VideoRecorderComponent.js
+++ src/components/Common/VideoRecorderComponent.js
@@ -8,11 +8,14 @@ const videoOptions = {
   mimeType: "video/mp4",
   disableLogs: true,
   frameRate: 60,
-  recordingLength: 30
+  recordingLength: 30,
 }
 
-const options= { fileName: 'patientVideo.mp4', fileType: 'mp4' , mimeType: "video/mp4"}
-
+const options = {
+  fileName: "patientVideo.mp4",
+  fileType: "mp4",
+  mimeType: "video/mp4",
+}
 
 const VideoPlayerComponent = ({ props, isOpen, onFileGenerated }) => {
   // const recordWebcam = useRecordWebcam(OPTIONS);
@@ -21,8 +24,8 @@ const VideoPlayerComponent = ({ props, isOpen, onFileGenerated }) => {
     height: 0,
     duration: 0,
   })
-  const [curRecording, setCurRecording]  = useState(null)
-  const [recorded, setRecorded]  = useState(null)
+  const [curRecording, setCurRecording] = useState(null)
+  const [recorded, setRecorded] = useState(null)
   const {
     activeRecordings,
     cancelRecording,
@@ -41,36 +44,38 @@ const VideoPlayerComponent = ({ props, isOpen, onFileGenerated }) => {
     resumeRecording,
     startRecording,
     stopRecording,
-  } = useRecordWebcam({options: options,
-     mediaRecorderOptions: { mimeType: 'video/mp4' }} )   
-   //{options: options,
+  } = useRecordWebcam({
+    options: options,
+    mediaRecorderOptions: { mimeType: "video/mp4" },
+  })
+  //{options: options,
   //   mediaRecorderOptions: { mimeType: 'video/mp4' },
   //   mediaTrackConstraints: { video: true, audio: true ,frameRate: 60 }
   //}
 
-
-console.log("devicesById", devicesById)
-  // const webcamRef = useRef(null);
-
-  useEffect(async () => {
+  useEffect(() => {
     if (isOpen) {
       // recordWebcam.open();
-      await start()
+      setTimeout(() => {
+         start()
+      }, 100); // small delay to let layout settle
+     
     }
 
     if (!isOpen) {
       // recordWebcam.close();
+      clearError(curRecording.id)
       clearPreview(curRecording.id)
       closeCamera(curRecording.id)
-
-
+      clearAllRecordings()
+      setCurRecording(null)
+      setRecorded(null)
     }
   }, [isOpen])
 
-
   const start = async () => {
     const recording = await createRecording()
-    setCurRecording(recording);
+    setCurRecording(recording)
     if (recording) await openCamera(recording.id)
   }
 
@@ -81,9 +86,11 @@ console.log("devicesById", devicesById)
         const {width, height} = stream.getVideoTracks()[0].getSettings(); */
     console.log(`${dimensions.width}x${dimensions.height}`)
 
-    const blob = recorded.blob;  //await recordWebcam.getRecording()
+    const blob = recorded.blob //await recordWebcam.getRecording()
     console.log("blob", blob)
-    var file = new File([blob], "patientVideo.mp4", { type: videoOptions.mimeType })
+    var file = new File([blob], "patientVideo.mp4", {
+      type: videoOptions.mimeType,
+    })
     Object.assign(file, {
       preview: URL.createObjectURL(file),
     })
@@ -123,16 +130,16 @@ console.log("devicesById", devicesById)
     clearPreview(curRecording.id)
     startRecording(curRecording.id)
 
-   // onFileGenerated()
+    // onFileGenerated()
   }
 
   function startButtonVisible(recording) {
     return (
-        recording.status !== "CLOSED" &&
-        recording.status !== "RECORDING" &&
-        // recording.status !== "PREVIEW" &&
-        recording.status !== "INITIAL" &&
-        ! recording.previewRef.current?.src.startsWith("blob:")
+      recording.status !== "CLOSED" &&
+      recording.status !== "RECORDING" &&
+      // recording.status !== "PREVIEW" &&
+      recording.status !== "INITIAL" &&
+      !recording.previewRef.current?.src.startsWith("blob:")
     )
   }
 
@@ -146,16 +153,16 @@ console.log("devicesById", devicesById)
 
   function isPreview(recording) {
     return recording.status === "STOPPED"
-   // return recording.previewRef.current?.src.startsWith("blob:")
+    // return recording.previewRef.current?.src.startsWith("blob:")
   }
 
   function replayVideo() {
-    activeRecordings[0].previewRef.current.play()
+    curRecording.previewRef.current.play()
   }
 
   const handleLoadedMetadata = e => {
     const videoElement = e.target
-     console.log("videoElement", videoElement)
+    console.log("videoElement", videoElement)
     setDimensions({
       width: videoElement.videoWidth,
       height: videoElement.videoHeight,
@@ -164,125 +171,129 @@ console.log("devicesById", devicesById)
   }
 
   return (
-     <div>
+    //   <div className="my-2">
+    //   <p>{errorMessage ? `Error: ${errorMessage}` : ""}</p>
+    //  </div>
+    activeRecordings?.length > 0 && (
+      <div>
         {/* <Button onClick={start}>Open camera</Button> */}
-        <div className="my-2">
-        <p>{errorMessage ? `Error: ${errorMessage}` : ""}</p>
-       </div> 
-   
-       <div>
-        {activeRecordings?.map(recording => (
-          <div key={recording.id}>
+
+        {/* <div> */}
+        {/* {activeRecordings?.map(recording => (
+          <> */}
+        <div>
+          <video
+            ref={activeRecordings[0].webcamRef}
+            style={{
+              height: "auto",
+              width: "100%",
+              display: `${
+                activeRecordings[0].status === "OPEN" ||
+                activeRecordings[0].status === "RECORDING"
+                  ? "block"
+                  : "none"
+              }`,
+            }}
+            muted
+            autoPlay
+            playsInline
+          />
+
+          <div
+      className={`${
+        activeRecordings[0].previewRef.current?.src.startsWith("blob:")
+          ? "visible"
+          : "hidden"
+      }`}
+    > 
+          {/* <p>Preview</p> */}
+        
             <video
-              ref={recording.webcamRef}
+              ref={activeRecordings[0].previewRef}
+              onLoadedMetadata={handleLoadedMetadata}
               style={{
                 height: "auto",
                 width: "100%",
-                display: `${
-                  recording.status === "OPEN" ||
-                  recording.status === "RECORDING"
-                    ? "block"
-                    : "none"
-                }`,
+                display: "block",
               }}
-              muted
               autoPlay
               playsInline
             />
-
-            <div
-              className={`${
-                recording.previewRef.current?.src.startsWith("blob:")
-                  ? "visible"
-                  : "hidden"
-              }`}
-            >
-              {/* <p>Preview</p> */}
-              <video
-                ref={recording.previewRef}
-                onLoadedMetadata={handleLoadedMetadata}
-                style={{
-                  height: "auto",
-                  width: "100%",
-                  display: "block",
-                }}
-                autoPlay
-                playsInline
-              />
-            </div>
-
-            <div className="text-center">
-              {/* {isInit(recording) && ( */}
-                {/* <div className="m-5 p-6">
-                  <Spinner size="lg" color="success" />
-                </div> */}
-              {/* )} */}
-
-              {stopButtonIsVisible(recording) && (
-                <>
-                  <Button
-                    className="rounded-circle mt-3"
-                    color="danger"
-                    onClick={stopVideoRecording}
-                    style={{ height: "80px", width: "80px", border: "0" }}
-                  >
-                    <i className="fas fa-stop fa-2x"></i>
-                  </Button>
-                  <h3 className="text-muted text-center p-2">
-                    <Countdown
-                      durationInSeconds={videoOptions.recordingLength}
-                      onTimerFinish={stopVideoRecording}
-                    />
-                  </h3>
-                </>
-              )}
-
-              {startButtonVisible(recording) && (
-                <>
-                  <Button
-                    className="rounded-circle text-center mt-3"
-                    color="danger"
-                    onClick={startVideoRecording}
-                    style={{ height: "80px", width: "80px", border: "0" }}
-                  >
-                    <i className="fas fa-video fa-2x"></i>
-                  </Button>
-                  <h3 className="text-muted text-center p-2">
-                    {props.t("TapToStartRecording")}
-                  </h3>
-                </>
-              )}
-
-              {isPreview(recording) && (
-                <div className="m-3">
-                  <Button
-                    color="info"
-                    onClick={retakeVideo}
-                    className="btn-rounded px-5 me-4"
-                  >
-                    {props.t("RetakeVideo")}
-                  </Button>
-                  <Button
-                    color="primary"
-                    onClick={replayVideo}
-                    className="btn-rounded px-5 me-4"
-                  >
-                    {props.t("ReplayVideo")}
-                  </Button>
-                  <Button
-                    color="success"
-                    onClick={saveFile}
-                    className="btn-rounded px-5"
-                  >
-                    {props.t("Save")}
-                  </Button>
-                </div>
-              )}
-            </div>
           </div>
-        ))}
+        </div>
+
+        <div className="text-center">
+
+        {isInit(activeRecordings[0]) &&
+                    <div className="m-5 p-6">
+                        <Spinner size="lg" color="success" />
+                    </div>
+                }
+
+          {stopButtonIsVisible(activeRecordings[0]) && (
+            <>
+              <Button
+                className="rounded-circle mt-3"
+                color="danger"
+                onClick={stopVideoRecording}
+                style={{ height: "80px", width: "80px", border: "0" }}
+              >
+                <i className="fas fa-stop fa-2x"></i>
+              </Button>
+              <h3 className="text-muted text-center p-2">
+                <Countdown
+                  durationInSeconds={videoOptions.recordingLength}
+                  onTimerFinish={stopVideoRecording}
+                />
+              </h3>
+            </>
+          )}
+
+          {startButtonVisible(activeRecordings[0]) && (
+            <>
+              <Button
+                className="rounded-circle text-center mt-3"
+                color="danger"
+                onClick={startVideoRecording}
+                style={{ height: "80px", width: "80px", border: "0" }}
+              >
+                <i className="fas fa-video fa-2x"></i>
+              </Button>
+              <h3 className="text-muted text-center p-2">
+                {props.t("TapToStartRecording")}
+              </h3>
+            </>
+          )}
+
+          {isPreview(activeRecordings[0]) && (
+            <div className="m-3">
+              <Button
+                color="info"
+                onClick={retakeVideo}
+                className="btn-rounded px-5 me-4"
+              >
+                {props.t("RetakeVideo")}
+              </Button>
+              <Button
+                color="primary"
+                onClick={replayVideo}
+                className="btn-rounded px-5 me-4"
+              >
+                {props.t("ReplayVideo")}
+              </Button>
+              <Button
+                color="success"
+                onClick={saveFile}
+                className="btn-rounded px-5"
+              >
+                {props.t("Save")}
+              </Button>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    )
+    // </div>
   )
 }
 
